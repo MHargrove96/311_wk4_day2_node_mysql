1763ae4e300abe47cb4ed4e6d5c746dff4f24693
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2022-03-08 17:48:13 -0600

    All methodsa are functional between vs code and postmate to preform the CRUD ops in my database

--- controllers/users.js
+++ controllers/users.js
@@ -1,67 +1,90 @@
-const mysql = require('mysql')
-const pool = require('../sql/connection')
-const { handleSQLError } = require('../sql/error')
+const mysql = require("mysql");
+const pool = require("../sql/connection");
+// POOL is what we get when you have a succsesful connection to MySQL.
+const { handleSQLError } = require("../sql/error");
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
   pool.query("SELECT * FROM users", (err, rows) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json(rows);
-  })
-}
+  });
+};
+//DONE
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  const { id } = req.params;
+  console.log(id);
+  let sql = `SELECT * FROM ?? WHERE ?? = ?`;
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["users", "id", id]);
 
   pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
+    console.log(err);
     return res.json(rows);
-  })
-}
+  });
+};
+//DONE
 
 const createUser = (req, res) => {
-  // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  // INSERT INTO USERS FIRST AND LAST NAME
+  let sql = "INSERT INTO ?? SET ?";
+  let values = {
+    ...req.body,
+  };
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["users", values]);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
+    console.log(err);
     return res.json({ newId: results.insertId });
-  })
-}
+  });
+};
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
-  // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  const { id } = req.params;
+  console.log(id, "IM THE ID IN THE UPDATE USER BY ID FUNCTION");
+  const { first_name, last_name } = req.body;
+  let sql = "UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?";
+  console.log(sql);
+  sql = mysql.format(sql, [
+    "users",
+    "first_name",
+    first_name,
+    "last_name",
+    last_name,
+    "id",
+    id,
+  ]);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
+    console.log(err);
     return res.status(204).json();
-  })
-}
+  });
+};
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let { firstname } = req.params;
+  let sql = "DELETE FROM ?? WHERE ?? = ?";
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["users", "first_name", firstname]);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-  })
-}
+  });
+};
 
 module.exports = {
   getAllUsers,
   getUserById,
   createUser,
   updateUserById,
-  deleteUserByFirstName
-}
+  deleteUserByFirstName,
+};
--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,29 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
+
+require("dotenv").config();
+// console.log(process.env);
+
+const { BD_HOST, BD_USER, BD_PASSWORD, BD_DATABASE } = process.env;
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating connection...')
+      console.log("creating connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+        host: BD_HOST,
+        user: BD_USER,
+        password: BD_PASSWORD,
+        database: BD_DATABASE,
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
